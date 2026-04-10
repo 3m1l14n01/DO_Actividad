d0f782baafc8e7c111bae094e2f28a780810d72e
3m1l14n01 <dravi95708977@gmail.com>
2026-04-09 18:40:20 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 AWS_REGION="us-east-2"
-S3_BUCKET="mi-bucket-deploy"   # Ajusta con el nombre real de tu bucket S3
+S3_BUCKET="codepipeline-us-east-2-5bd13784c431-41b7-92b0-2ef4594a151e"   # Ajusta con el nombre real de tu bucket S3
 
 echo "Copiando artefactos al bucket..."
 aws s3 cp ./app s3://$S3_BUCKET/ --recursive --region $AWS_REGION
